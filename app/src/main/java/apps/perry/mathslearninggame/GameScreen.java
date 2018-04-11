e7a2f5945ed11120088bb7ae18b6a9c6a96d1a01
Perry Hartono <phar2413@uni.sydney.edu.au>
2018-04-11 19:37:01 +1000

    Added new question layouts

--- app/src/main/java/apps/perry/mathslearninggame/GameScreen.java
+++ app/src/main/java/apps/perry/mathslearninggame/GameScreen.java
@@ -2,12 +2,26 @@ package apps.perry.mathslearninggame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameScreen extends AppCompatActivity {
+
+    boolean sw = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_screen);
+        setContentView(R.layout.activity_game_screen_mc);
+    }
+
+    public void switchView(View v) {
+        if (sw) {
+            setContentView(R.layout.activity_game_screen_fii);
+        }
+        else {
+            setContentView(R.layout.activity_game_screen_mc);
+        }
+
+        sw = !sw;
     }
 }
